c0c78e95554a0f21c2997d7ddd525002b1ffe959
TEERAWAT PRAROM <teewat@yahoo.com>
2022-03-03 06:47:04 +1100

    completed nassignment

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,15 +1,19 @@
 export default function counterReducer(
-	state = {
-		clicks: 0
-	},
-	action
+  state = {
+    clicks: 0,
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				clicks: state.clicks + 1
-			}
-		default:
-			return state;
-	}
+  console.log(action);
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      console.log("Current state.clicks %s", state.clicks);
+      console.log("Updating state.clicks to %s", state.clicks + 1);
+      return {
+        clicks: state.clicks + 1,
+      };
+    default:
+      console.log("Initial state.clicks: %s", state.clicks);
+      return state;
+  }
 }
